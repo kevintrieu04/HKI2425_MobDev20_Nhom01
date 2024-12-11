66ef3ef9d06c58580871d289400348e123b2a4cd
trung <22028170@vnu.edu.vn>
2024-12-11 21:09:11 +0700

    moviedb

---
+++ backend/themoviedb/themoviedb-master/examples/async_search.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+from themoviedb import aioTMDb
+
+
+async def main():
+    tmdb = aioTMDb(key="YOUR_API_KEY")
+    results = await tmdb.search().multi("jack")
+    for result in results:
+        if result.is_movie():
+            movie = await tmdb.movie(result.id).details()
+            print(movie, movie.overview, "\n")
+        elif result.is_person():
+            person = await tmdb.person(result.id).details()
+            print(person, person.biography, "\n")
+        elif result.is_tv():
+            tv = await tmdb.tv(result.id).details()
+            print(tv, tv.overview, "\n")
+
+
+asyncio.run(main())
---
+++ backend/themoviedb/themoviedb-master/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import os
+
+os.environ["TMDB_KEY"] = "TEST_TMDB_KEY"
+os.environ["TMDB_LANGUAGE"] = "TEST_TMDB_LANGUAGE"
+os.environ["TMDB_REGION"] = "TEST_TMDB_REGION"
